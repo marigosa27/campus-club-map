d412e4b101a3360025bfa508f604bfa41a709775
Maria G Saldarriaga <marigosa96@gmail.com>
2026-07-20 12:03:46 -0400

    Remove incorrect Fischer entrance points

--- layers/entrances_exit_4.js
+++ layers/entrances_exit_4.js
@@ -1,1 +1,525 @@
-var json_entrances_exit_4 = {"type":"FeatureCollection","name":"entrances_exit_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"2","name":"Main entrance","building":"Ogden","type":"entrance","accessibi":"no wheelchair accessible","hours":"7am-6pm","notes":"hint where the entrance is"},"geometry":{"type":"Point","coordinates":[-74.013392834268927,41.422676414446343]}},{"type":"Feature","properties":{"id":"3","name":"Middle Entrance","building":"McConnell","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.01275521441022,41.424236950380852]}},{"type":"Feature","properties":{"id":"1","name":"East Entrance","building":"Dempsy","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.012476792800754,41.424261011507596]}},{"type":"Feature","properties":{"id":"4","name":"Entrance","building":"Stone","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.013277684590932,41.423109514727727]}},{"type":"Feature","properties":{"id":"5","name":"East Entrance","building":"Dyer","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.012896143866854,41.422353307887221]}},{"type":"Feature","properties":{"id":"6","name":"West Entrance","building":"Dyer","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.013181440083954,41.422535484989709]}},{"type":"Feature","properties":{"id":"7","name":"West Entrance","building":"Modular Mod","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.013078320969342,41.422370494406323]}},{"type":"Feature","properties":{"id":"8","name":"East Entrance","building":"Modular Mod","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.013215813122159,41.422446115090374]}},{"type":"Feature","properties":{"id":"9","name":"North Entrance","building":"High mount","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.013092070184626,41.421978641770785]}},{"type":"Feature","properties":{"id":"10","name":"South Entrance","building":"High mount","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.013037928967847,41.421830195973229]}},{"type":"Feature","properties":{"id":"12","name":"Exit","building":"Ogden","type":"Exit","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.013431182880865,41.422768203257554]}},{"type":"Feature","properties":{"id":"11","name":"North Entrance","building":"admissions","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.012338452745524,41.422320151668124]}},{"type":"Feature","properties":{"id":"13","name":"North Entrance","building":"Business office","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.012041900614392,41.422304034704482]}},{"type":"Feature","properties":{"id":"14","name":"Exit","building":"Business office","type":"Exit","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.012135379003553,41.422307258097206]}},{"type":"Feature","properties":{"id":null,"name":"North Entrance","building":"Stillman","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.012332005960062,41.42204293989338]}},{"type":"Feature","properties":{"id":"15","name":"South Entrance","building":"Stillman","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.01261244112753,41.421926897755114]}},{"type":"Feature","properties":{"id":"16","name":"North Entrance","building":"Gym","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.011642199915926,41.422017152751543]}},{"type":"Feature","properties":{"id":"18","name":"South Entrance","building":"Gym","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.011590625632252,41.421904334006008]}},{"type":"Feature","properties":{"id":"17","name":"West Entrance","building":"Theater","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.011316637250232,41.421943014718764]}},{"type":"Feature","properties":{"id":"21","name":"North Entrance","building":"Theater","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.011284403322932,41.422165428817102]}},{"type":"Feature","properties":{"id":"25","name":"North Entrance","building":"Cottage","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.010852468697166,41.42237494934453]}},{"type":"Feature","properties":{"id":"20","name":"Student Center","building":"dinning hall","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.012560866843856,41.423251712166987]}},{"type":"Feature","properties":{"id":"18","name":"Student center","building":"dinning hall","type":"Exit","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.012261091320013,41.423271052523368]}},{"type":"Feature","properties":{"id":"19","name":"dinning hall","building":"dinning hall","type":"Both","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.012280431676388,41.423087319137778]}},{"type":"Feature","properties":{"id":"19","name":"Entrance","building":"Tennis Court","type":"entrance","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.012016113472555,41.423525700549007]}},{"type":"Feature","properties":{"id":"23","name":"West entrance","building":"McConnell","type":"Entrance","accessibi":null,"hours":null,"notes":null},"geometry":{"type":"Point","coordinates":[-74.012960567542308,41.424221953378606]}},{"type":"Feature","properties":{"id":"15","name":"northwest entrance","building":"art center","type":"entrance and exit","accessibi":"no","hours":"7:30-2:45","notes":null},"geometry":{"type":"Point","coordinates":[-74.013855264975675,41.421472849256624]}},{"type":"Feature","properties":{"id":"18","name":"northeast entrance","building":"art center","type":"entrance and exit","accessibi":"no","hours":"7:30-2:45","notes":null},"geometry":{"type":"Point","coordinates":[-74.013593802684426,41.421411049078692]}},{"type":"Feature","properties":{"id":"19","name":"southwest entance","building":"art center","type":"entrance and exit","accessibi":"yes","hours":"7:30-2:45","notes":null},"geometry":{"type":"Point","coordinates":[-74.013764941638698,41.421382525919647]}}]}
+var json_entrances_exit_4 = {
+  "type": "FeatureCollection",
+  "name": "entrances_exit_4",
+  "crs": {
+    "type": "name",
+    "properties": {
+      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+    }
+  },
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "2",
+        "name": "Main entrance",
+        "building": "Ogden",
+        "type": "entrance",
+        "accessibi": "no wheelchair accessible",
+        "hours": "7am-6pm",
+        "notes": "hint where the entrance is"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01339283426893,
+          41.42267641444634
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "3",
+        "name": "Middle Entrance",
+        "building": "McConnell",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01275521441022,
+          41.42423695038085
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "1",
+        "name": "East Entrance",
+        "building": "Dempsy",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01247679280075,
+          41.424261011507596
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "4",
+        "name": "Entrance",
+        "building": "Stone",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01327768459093,
+          41.42310951472773
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "5",
+        "name": "East Entrance",
+        "building": "Dyer",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01289614386685,
+          41.42235330788722
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "6",
+        "name": "West Entrance",
+        "building": "Dyer",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01318144008395,
+          41.42253548498971
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "9",
+        "name": "North Entrance",
+        "building": "High mount",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01309207018463,
+          41.421978641770785
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "10",
+        "name": "South Entrance",
+        "building": "High mount",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01303792896785,
+          41.42183019597323
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "12",
+        "name": "Exit",
+        "building": "Ogden",
+        "type": "Exit",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01343118288086,
+          41.422768203257554
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "11",
+        "name": "North Entrance",
+        "building": "admissions",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01233845274552,
+          41.422320151668124
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "13",
+        "name": "North Entrance",
+        "building": "Business office",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01204190061439,
+          41.42230403470448
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "14",
+        "name": "Exit",
+        "building": "Business office",
+        "type": "Exit",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01213537900355,
+          41.422307258097206
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": null,
+        "name": "North Entrance",
+        "building": "Stillman",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01233200596006,
+          41.42204293989338
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "15",
+        "name": "South Entrance",
+        "building": "Stillman",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01261244112753,
+          41.421926897755114
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "16",
+        "name": "North Entrance",
+        "building": "Gym",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01164219991593,
+          41.42201715275154
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "18",
+        "name": "South Entrance",
+        "building": "Gym",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01159062563225,
+          41.42190433400601
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "17",
+        "name": "West Entrance",
+        "building": "Theater",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01131663725023,
+          41.421943014718764
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "21",
+        "name": "North Entrance",
+        "building": "Theater",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01128440332293,
+          41.4221654288171
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "25",
+        "name": "North Entrance",
+        "building": "Cottage",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01085246869717,
+          41.42237494934453
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "20",
+        "name": "Student Center",
+        "building": "dinning hall",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01256086684386,
+          41.42325171216699
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "18",
+        "name": "Student center",
+        "building": "dinning hall",
+        "type": "Exit",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01226109132001,
+          41.42327105252337
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "19",
+        "name": "dinning hall",
+        "building": "dinning hall",
+        "type": "Both",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01228043167639,
+          41.42308731913778
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "19",
+        "name": "Entrance",
+        "building": "Tennis Court",
+        "type": "entrance",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01201611347255,
+          41.42352570054901
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "23",
+        "name": "West entrance",
+        "building": "McConnell",
+        "type": "Entrance",
+        "accessibi": null,
+        "hours": null,
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01296056754231,
+          41.424221953378606
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "15",
+        "name": "northwest entrance",
+        "building": "art center",
+        "type": "entrance and exit",
+        "accessibi": "no",
+        "hours": "7:30-2:45",
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01385526497567,
+          41.421472849256624
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "18",
+        "name": "northeast entrance",
+        "building": "art center",
+        "type": "entrance and exit",
+        "accessibi": "no",
+        "hours": "7:30-2:45",
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.01359380268443,
+          41.42141104907869
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "19",
+        "name": "southwest entance",
+        "building": "art center",
+        "type": "entrance and exit",
+        "accessibi": "yes",
+        "hours": "7:30-2:45",
+        "notes": null
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.0137649416387,
+          41.42138252591965
+        ]
+      }
+    }
+  ]
+};
